5814654ef5684d81c70e3933486f895187b803d7
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-10-17 08:45:16 +0100

    making little changes

--- app/src/main/java/com/example/nuntium/ui/screens/signIn/signInScreen.kt
+++ app/src/main/java/com/example/nuntium/ui/screens/signIn/signInScreen.kt
@@ -1,5 +1,6 @@
 package com.example.nuntium.ui.screens.signIn
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.RemoveRedEye
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,122 +32,130 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.nuntium.R
 import com.example.nuntium.ui.common.CustomButton
+import com.example.nuntium.ui.common.CustomClickableText
 import com.example.nuntium.ui.common.CustomSignInButton
-import com.example.nuntium.ui.common.CustomSignInText
 import com.example.nuntium.ui.common.CustomTextField
 import com.example.nuntium.ui.theme.SfProFontFamily
 
 @Composable
-fun signInScreen() {
+fun SignInScreen() {
     val focusManager = LocalFocusManager.current
     Column(
         modifier = Modifier
-            .padding(start = 20.dp, end = 20.dp, top = 28.dp)
-            .fillMaxSize()
+            .padding(start = 20.dp, end = 20.dp, top = 28.dp, bottom = 8.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(
-            modifier = Modifier.padding(top = 0.dp),
-            text = "Welcome Back \uD83D\uDC4B",
-            fontFamily = SfProFontFamily,
-            fontWeight = FontWeight.W600,
-            fontSize = 24.sp,
-            color = MaterialTheme.colorScheme.onBackground
-        )
-        Text(
-            modifier = Modifier
-                .padding(top = 8.dp),
-            text = stringResource(R.string.welcome_message),
-            fontFamily = SfProFontFamily,
-            fontWeight = FontWeight.W400,
-            fontSize = 16.sp,
-            color = MaterialTheme.colorScheme.tertiary
-        )
-        Spacer(modifier = Modifier.height(32.dp))
-        CustomTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            onValueChange = { },
-            placeholder = stringResource(R.string.email_placeholder),
-            leadingIcon = Icons.Outlined.Email,
-            keyboardOptions = KeyboardOptions(
-                autoCorrect = true,
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Next
-            ),
-            keyboardAction = KeyboardActions(
-                onNext = { focusManager.moveFocus(FocusDirection.Next) }
-            )
-        )
-        CustomTextField(
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .fillMaxWidth(),
-            value = "",
-            onValueChange = { },
-            placeholder = stringResource(R.string.password_placeholder),
-            leadingIcon = Icons.Outlined.Lock,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done
-            ),
-            keyboardAction = KeyboardActions(
-                onDone = { focusManager.clearFocus() }
-            )
-        )
-        ClickableText(
-            modifier = Modifier
-                .padding(top = 16.dp, bottom = 24.dp)
-                .fillMaxWidth(),
-            text = AnnotatedString("Forgot Password?"),
-            onClick = { },
-            style = TextStyle(
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(
+                modifier = Modifier.padding(top = 0.dp),
+                text = "Welcome Back \uD83D\uDC4B",
                 fontFamily = SfProFontFamily,
-                textAlign = TextAlign.Right,
-                color = MaterialTheme.colorScheme.tertiary,
-                fontWeight = FontWeight.W500,
-                fontSize = 16.sp
+                fontWeight = FontWeight.W600,
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onBackground
             )
-        )
-        CustomButton(
-            modifier = Modifier.fillMaxWidth(),
-            text = stringResource(R.string.sign_in),
-            onClick = { }
-        )
-        Text(
-            modifier = Modifier
-                .padding(vertical = 48.dp)
-                .fillMaxWidth(),
-            text = stringResource(R.string.or),
-            textAlign = TextAlign.Center
-        )
-        CustomSignInButton(
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-                .fillMaxWidth(),
-            text = "Sign In with Google",
-            icon = R.drawable.google_logo,
-            onClick = {}
-        )
-        CustomSignInButton(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Sign In with Facebook",
-            icon = R.drawable.facebook_logo,
-            onClick = {}
-        )
+            Text(
+                modifier = Modifier
+                    .padding(top = 8.dp),
+                text = stringResource(R.string.welcome_message),
+                fontFamily = SfProFontFamily,
+                fontWeight = FontWeight.W400,
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            CustomTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = { /*TODO*/ },
+                placeholder = stringResource(R.string.email_placeholder),
+                leadingIcon = Icons.Outlined.Email,
+                keyboardOptions = KeyboardOptions(
+                    autoCorrect = true,
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardAction = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Next) }
+                )
+            )
+            CustomTextField(
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth(),
+                value = "",
+                onValueChange = { /*TODO*/ },
+                placeholder = stringResource(R.string.password_placeholder),
+                leadingIcon = Icons.Outlined.Lock,
+                trailingIcon = Icons.Outlined.RemoveRedEye,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardAction = KeyboardActions(
+                    onDone = { focusManager.clearFocus() }
+                )
+            )
+            ClickableText(
+                modifier = Modifier
+                    .padding(top = 16.dp, bottom = 24.dp)
+                    .fillMaxWidth(),
+                text = AnnotatedString(stringResource(id = R.string.forgot_password_question)),
+                onClick = { /*TODO*/ },
+                style = TextStyle(
+                    fontFamily = SfProFontFamily,
+                    textAlign = TextAlign.Right,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontWeight = FontWeight.W500,
+                    fontSize = 16.sp
+                )
+            )
+            CustomButton(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(R.string.sign_in),
+                onClick = { /*TODO*/ }
+            )
+            Text(
+                modifier = Modifier
+                    .padding(vertical = 48.dp)
+                    .fillMaxWidth(),
+                text = stringResource(R.string.or),
+                textAlign = TextAlign.Center
+            )
+            CustomSignInButton(
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .fillMaxWidth(),
+                text = "Sign In with Google",
+                icon = R.drawable.google_logo,
+                onClick = { /*TODO*/ }
+            )
+            CustomSignInButton(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Sign In with Facebook",
+                icon = R.drawable.facebook_logo,
+                onClick = { /*TODO*/ }
+            )
+        }
 
-        CustomSignInText(
+        CustomClickableText(
             modifier = Modifier
-            .padding(top = 50.dp)
-            .fillMaxWidth(),
-            onCLick = {  }
+                .padding(top = 50.dp)
+                .fillMaxWidth(),
+            firstText = stringResource(R.string.don_t_have_an_account),
+            secondText = stringResource(R.string.sign_up),
+            onCLick = { /*TODO*/ }
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun signInScreenPreview(){
+fun SignInScreenPreview(){
     MaterialTheme {
-        signInScreen()
+        SignInScreen()
     }
 }
