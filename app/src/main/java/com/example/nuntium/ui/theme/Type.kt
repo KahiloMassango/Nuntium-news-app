2835dc31f52dac3d0a53c2c1b2189fa8f930c6e6
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-10-20 12:22:35 +0100

    adding a new typography

--- app/src/main/java/com/example/nuntium/ui/theme/Type.kt
+++ app/src/main/java/com/example/nuntium/ui/theme/Type.kt
@@ -1,5 +1,6 @@
 package com.example.nuntium.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -15,27 +16,25 @@ val SfProFontFamily = FontFamily(
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = SfProFontFamily,
+        fontWeight = FontWeight.W700,
+        fontSize = 24.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    bodyLarge = TextStyle(
+        fontFamily = SfProFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 20.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = SfProFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 16.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = SfProFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
     )
-    */
+
 )
