3f4cf92a493359177df1950748062ae8f8b129e6
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-10-20 11:14:32 +0100

    default padding for the whole app

---
+++ app/src/main/java/com/example/nuntium/ui/common/CustomPadding.kt
@@ -0,0 +1,10 @@
+package com.example.nuntium.ui.common
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.unit.dp
+
+val defaultPadding = PaddingValues(
+    start = 20.dp,
+    end = 20.dp,
+    top = 28.dp
+)
