3e30c05fcd21712fe2eebcd4f82f2bdba4880f95
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-11-05 13:44:50 +0100

    creating database with room

---
+++ app/src/main/java/com/example/nuntium/data/database/AppDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.nuntium.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.nuntium.data.model.ArticleDto
+
+@Database(entities = [ArticleDto::class], version = 2)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun articleDao(): ArticleDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(context = context, AppDatabase::class.java, "article_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
+
+
+}
