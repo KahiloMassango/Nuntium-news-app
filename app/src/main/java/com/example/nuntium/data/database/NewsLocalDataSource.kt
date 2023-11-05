731cecd5fc9760c02e3f471849e6a8186457ddcc
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-11-05 14:00:54 +0100

    creating local and remote data sources

---
+++ app/src/main/java/com/example/nuntium/data/database/NewsLocalDataSource.kt
@@ -0,0 +1,35 @@
+package com.example.nuntium.data.database
+
+import com.example.nuntium.data.model.ArticleDto
+import kotlinx.coroutines.flow.Flow
+
+interface NewsLocalDataSource {
+
+    suspend fun insertArticle(article: ArticleDto)
+
+    suspend fun deleteArticle(article: ArticleDto)
+
+    suspend fun getArticleById(id: Int): ArticleDto
+
+    fun getAll(): Flow<List<ArticleDto>>
+}
+
+class NewsLocalDataSourceImpl(
+    private val articleDao: ArticleDao
+): NewsLocalDataSource {
+    override suspend fun insertArticle(article: ArticleDto) {
+        articleDao.insertArticle(article)
+    }
+
+    override suspend fun deleteArticle(article: ArticleDto) {
+        articleDao.deleteArticle(article)
+    }
+
+    override suspend fun getArticleById(id: Int): ArticleDto {
+        return articleDao.getArticleById(id)
+    }
+
+    override fun getAll(): Flow<List<ArticleDto>> {
+        return articleDao.getAll()
+    }
+}
---
+++ app/src/main/java/com/example/nuntium/data/network/NewsRemoteDataSource.kt
@@ -0,0 +1,28 @@
+package com.example.nuntium.data.network
+
+import com.example.nuntium.data.model.Article
+
+interface NewsRemoteDataSource {
+    suspend fun fetchNewsByCategory(category: String): List<Article>
+
+    suspend fun fetchNewsByKeyword(keyword: String): List<Article>
+
+    suspend fun fetchLatestNews(): List<Article>
+}
+
+class NewsRemoteDataSourceImpl(
+    private val newsApi: NewsApiService
+): NewsRemoteDataSource {
+    override suspend fun fetchNewsByCategory(category: String): List<Article> {
+        return newsApi.fetchNewsByCategory(category.lowercase()).articles
+    }
+
+    override suspend fun fetchNewsByKeyword(keyword: String): List<Article> {
+        return newsApi.fetchNewsByKeyword(keyword).articles
+    }
+
+    override suspend fun fetchLatestNews(): List<Article> {
+        return newsApi.fetchLatestNews().articles
+    }
+
+}
