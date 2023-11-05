b704079bdc20b2df474d0a199da876c76821a4aa
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-11-05 14:01:33 +0100

    creating news repository and implementing local and remote data source in it

---
+++ app/src/main/java/com/example/nuntium/data/repository/NewsRepository.kt
@@ -0,0 +1,57 @@
+package com.example.nuntium.data.repository
+
+import com.example.nuntium.data.database.NewsLocalDataSource
+import com.example.nuntium.data.model.Article
+import com.example.nuntium.data.model.ArticleDto
+import com.example.nuntium.data.network.NewsRemoteDataSource
+import kotlinx.coroutines.flow.Flow
+
+interface NewsRepository {
+    suspend fun saveLocalArticle(article: ArticleDto)
+
+    suspend fun deleteLocalArticle(article: ArticleDto)
+
+    suspend fun getLocalArticleById(id: Int): ArticleDto
+
+     fun getAllLocalNews(): Flow<List<ArticleDto>>
+
+    suspend fun fetchRemoteNewsByCategory(category: String): List<Article>
+
+    suspend fun fetchRemoteNewsByKeyword(keyword: String): List<Article>
+
+    suspend fun fetchRemoteLatestNews(): List<Article>
+
+}
+
+class NewsRepositoryImpl(
+    private val localDataSource: NewsLocalDataSource,
+    private val remoteDataSource: NewsRemoteDataSource
+): NewsRepository {
+    override suspend fun saveLocalArticle(article: ArticleDto) {
+        localDataSource.insertArticle(article)
+    }
+
+    override suspend fun deleteLocalArticle(article: ArticleDto) {
+        localDataSource.deleteArticle(article)
+    }
+
+    override suspend fun getLocalArticleById(id: Int): ArticleDto {
+        return localDataSource.getArticleById(id)
+    }
+
+    override fun getAllLocalNews(): Flow<List<ArticleDto>> {
+        return localDataSource.getAll()
+    }
+
+    override suspend fun fetchRemoteNewsByCategory(category: String): List<Article> {
+        return remoteDataSource.fetchNewsByCategory(category)
+    }
+
+    override suspend fun fetchRemoteNewsByKeyword(keyword: String): List<Article> {
+        return remoteDataSource.fetchNewsByKeyword(keyword)
+    }
+
+    override suspend fun fetchRemoteLatestNews(): List<Article> {
+        return remoteDataSource.fetchLatestNews()
+    }
+}
