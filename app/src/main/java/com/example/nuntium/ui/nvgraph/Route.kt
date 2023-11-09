d3388ad06fc98291a4fdb1f1b6354d968d070cbf
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-11-09 21:08:44 +0100

    creating a list of screens that have bottomBar

--- app/src/main/java/com/example/nuntium/ui/nvgraph/Route.kt
+++ app/src/main/java/com/example/nuntium/ui/nvgraph/Route.kt
@@ -7,9 +7,19 @@ sealed class Route(
     data object MainScreens: Route("mainScreens")
     data object HomeScreen: Route("homeScreen")
     data object ArticleScreen: Route("articleScreen")
-    data object BookmarksScreen: Route("bookmarksScreen")
+    data object SavedScreen: Route("savedScreen")
     data object CategoryScreen: Route("categoryScreen")
     data object LanguageScreen: Route("languageScreen")
     data object PreferenceScreen: Route("preferenceScreen")
     data object WelcomeScreen: Route("welcomeScreen")
 }
+
+private val bottomBarScreens: List<Route> = listOf(
+    Route.HomeScreen,
+    Route.SavedScreen,
+    Route.CategoryScreen,
+    Route.PreferenceScreen
+)
+
+val bottomBarRoutes = bottomBarScreens.map { it.route }
+
