65c2f1447c3adba4d09561560730034eac988e34
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-11-11 07:31:54 +0100

    refactoring article viewmodel

--- app/src/main/java/com/example/nuntium/ui/screens/article/ArticleViewModel.kt
+++ app/src/main/java/com/example/nuntium/ui/screens/article/ArticleViewModel.kt
@@ -1,11 +1,33 @@
 package com.example.nuntium.ui.screens.article
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.nuntium.data.model.Article
+import com.example.nuntium.data.model.toArticleDto
 import com.example.nuntium.data.repository.NewsRepository
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
-class ArticleViewModel(private val newsRepository: NewsRepository): ViewModel() {
+class ArticleViewModel(
+    private val newsRepository: NewsRepository,
+    savedStateHandle: SavedStateHandle
+): ViewModel() {
 
-    
+    private val json = savedStateHandle.get<String>("Article")
+
+    private val articleObj = Gson().fromJson(json, Article::class.java)
+
+    val uiState: Article? by mutableStateOf(articleObj)
+
+    fun saveArticleLocally(article: Article){
+        viewModelScope.launch(Dispatchers.IO) {
+            newsRepository.saveLocalArticle(article.toArticleDto())
+        }
+    }
 
 }
